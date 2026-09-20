3c8a623b67172947c79bffdc2d67f26559c66295
Elie Saad <esaad.lb@gmail.com>
2026-09-21 00:58:10 +0300

    Register QAuth and q_flow clients

--- quollnet_mcp/dependencies.py
+++ quollnet_mcp/dependencies.py
@@ -1,4 +1,8 @@
 from quollnet_mcp.services.qapp_client import QAppClient
+from quollnet_mcp.services.qauth_client import QAuthClient
+from quollnet_mcp.services.qflow_client import QFlowClient
 
 
 qapp_client = QAppClient()
+qauth_client = QAuthClient()
+qflow_client = QFlowClient()
