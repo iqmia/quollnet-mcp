36c246d161d88aa3838372d043568e271210de19
Elie Saad <esaad.lb@gmail.com>
2026-09-24 16:16:16 +0300

    Add qApp target ID to tests/test_cashflowpot.py

--- tests/test_cashflowpot.py
+++ tests/test_cashflowpot.py
@@ -6,6 +6,7 @@ import unittest
 
 os.environ.setdefault("QAUTH_APP_ID", "test-app-id")
 os.environ.setdefault("QAUTH_PUBLIC_KEY", "test-public-key")
+os.environ.setdefault("QAPP_APP_ID", "qapp-test-app")
 os.environ.setdefault("CASHFLOWPOT_APP_ID", "cashflowpot-test-app")
 
 from mcp.server.mcpserver.exceptions import ToolError
