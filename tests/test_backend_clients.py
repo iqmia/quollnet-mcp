c7f78905e8aa8969a7dfa47b2b90b6c0e99d54d1
Elie Saad <esaad.lb@gmail.com>
2026-09-24 16:16:22 +0300

    Add qApp target ID to tests/test_backend_clients.py

--- tests/test_backend_clients.py
+++ tests/test_backend_clients.py
@@ -6,6 +6,7 @@ import httpx
 
 os.environ.setdefault("QAUTH_APP_ID", "test-app-id")
 os.environ.setdefault("QAUTH_PUBLIC_KEY", "test-public-key")
+os.environ.setdefault("QAPP_APP_ID", "qapp-test-app")
 os.environ.setdefault("CASHFLOWPOT_APP_ID", "cashflowpot-test-app")
 
 from quollnet_mcp.services.qapp_client import QAppClient
